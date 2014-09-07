a14bc1285473ad7706479c0e0774cad4aedba7a2
Pauli B <pauli.buccini@gmail.com>
2014-09-07 22:29:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+meteor-linkedin
+===============
+Forked from https://github.com/yefim/meteor-linkedin.git
+
+A OAuth2 wrapper for the LinkedIn API
+
+##Installation
+
+* Install accounts-ui pacakge: `meteor add accounts-ui`
+* `meteor add pauli:linkedin`
+
+##Usage
+
+Creates a new function `Meteor.loginWithLinkedin(options, callback)`
+
+This is the backbone of `pauli:accounts-linkedin`
---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summary: "A OAuth2 wrapper for the LinkedIN API",
+  version: "1.0.0",
+  git: "https://github.com/PauliBuccini/meteor-linkedin.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+
+  api.use('oauth@1.0.1', ['client', 'server']);
+  api.use('oauth2@1.0.0', ['client', 'server']);
+  api.use('http@1.0.4', ['client', 'server']);
+  api.use('templating@1.0.5', 'client');
+  api.use('service-configuration@1.0.5', ['client', 'server']);
+
+  api.export('LinkedIn');
+
+  api.addFiles(
+    ['linkedin_configure.html', 'linkedin_configure.js'],
+    'client');
+  api.addFiles('linkedin_common.js', ['client', 'server']);
+  api.addFiles('linkedin_server.js', 'server');
+  api.addFiles('linkedin_client.js', 'client');
+});
+
